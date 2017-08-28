86c41313e85167eaf7fc85c2ee459ce52e3796d3
Cody Biller <sharpeleven91@gmail.com>
2017-08-28 11:27:59 -0400

    add partial javascript functionality to search

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+
+    var search = document.getElementById("search-get");
+
+    search.onclick = function() {
+        var userInput = document.getElementById("userInput").value;
+
+    }
+}
