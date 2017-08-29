570370e6cb9122e7197ca74bc0cad802e8e2d9fa
Cody Biller <sharpeleven91@gmail.com>
2017-08-29 12:48:34 -0400

    fix styling issues

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,8 @@ $("#search-get").click(function () {
     var userInput = document.getElementById("userInput").value;
 	$.getJSON(apiQuery + userInput, function(data) {
         var results = data.query.pages;
-
+        
+        //iterate with JSON data and append values to HTML document
         Object.keys(results).forEach(function(key) {
             $("#content").append("<div class='results'> <a href='https://en.wikipedia.org/?curid=" + results[key].pageid +"'> <h3 class='title'>" 
                                                                              + results[key].title + "</h3> <br> <p class='extract'>" 
