89505d7ddead4b953262042231f68bccd146bd0b
Cody Biller <sharpeleven91@gmail.com>
2017-08-29 12:11:18 -0400

    complete search functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,14 @@
-window.onload = function () {
+var apiQuery = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&origin=*&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+$("#search-get").click(function () {
+    $("#content").empty();
+    var userInput = document.getElementById("userInput").value;
+	$.getJSON(apiQuery + userInput, function(data) {
+        var results = data.query.pages;
 
-    var search = document.getElementById("search-get");
-
-    search.onclick = function() {
-        var userInput = document.getElementById("userInput").value;
-
-    }
-}
+        Object.keys(results).forEach(function(key) {
+            $("#content").append("<div class='results'> <a href='https://en.wikipedia.org/?curid=" + results[key].pageid +"'> <h3 class='title'>" 
+                                                                             + results[key].title + "</h3> <br> <p class='extract'>" 
+																             + results[key].extract + "</p> </div> </a>");
+        });
+	});
+});
